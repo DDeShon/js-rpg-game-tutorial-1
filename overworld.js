382b44da3a0a13c377028ccb32ec833845a517a5
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-28 20:01:27 -0500

    working on hero character image

--- overworld.js
+++ overworld.js
@@ -11,5 +11,11 @@ class Overworld {
       this.ctx.drawImage(image, 0, 0);
     };
     image.src = "/images/maps/DemoLower.png";
+
+    const hero = new Image();
+    hero.onload = () => {
+      this.ctx.drawImage(hero, 0, 0);
+    };
+    hero.src = "/images/characters/people/hero.png";
   }
 }
