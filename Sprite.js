d48f95d0bb128f5c309657bfa0206db12d1876c1
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-29 21:51:21 -0500

    working on sprite class

--- Sprite.js
+++ Sprite.js
@@ -2,12 +2,14 @@ class Sprite {
   constructor(config) {
     this.animations = config.animations || {
       idleDown: [[0, 0]],
-      walkDown: [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-      ],
+      //   walkDown: [
+      //     [0, 0],
+      //     [1, 0],
+      //     [2, 0],
+      //     [3, 0],
+      //   ],
     };
+    this.currentAnimation = config.currentAnimation || "idleDown";
+    this.currentAnimationFrame = 0;
   }
 }
