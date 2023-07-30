588d2659a38fcd8bf2ba6ada2f0d717d1f4ebacd
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-30 17:18:01 -0500

    updating Overworld.js

--- Sprite.js
+++ Sprite.js
@@ -28,6 +28,6 @@ class Sprite {
     const x = this.gameObject.x * 16 - 8;
     const y = this.gameObject.y * 16 - 18;
 
-    ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32);
+    this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 32, x, y, 32, 32);
   }
 }
