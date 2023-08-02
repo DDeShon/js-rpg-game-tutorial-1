9f21f350e338e8f9a280f9ce33b8d0bbb4672ee6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-01 22:15:18 -0500

    working on sprite animation

--- Sprite.js
+++ Sprite.js
@@ -20,14 +20,35 @@ class Sprite {
     // configure animation and initial state
     this.animations = config.animations || {
       "idle-down": [[0, 0]],
+      "idle-right": [[0, 1]],
+      "idle-up": [[0, 2]],
+      "idle-left": [[0, 3]],
       "walk-down": [
         [1, 0],
         [0, 0],
         [3, 0],
         [0, 0],
       ],
+      "walk-right": [
+        [1, 1],
+        [0, 1],
+        [3, 1],
+        [0, 1],
+      ],
+      "walk-up": [
+        [1, 2],
+        [0, 2],
+        [3, 2],
+        [0, 2],
+      ],
+      "walk-left": [
+        [1, 3],
+        [0, 3],
+        [3, 3],
+        [0, 3],
+      ],
     };
-    this.currentAnimation = "walk-down"; //config.currentAnimation || "idle-down";
+    this.currentAnimation = config.currentAnimation || "idle-down";
     this.currentAnimationFrame = 0;
 
     this.animationFrameLimit = config.animationFrameLimit || 16;
@@ -39,6 +60,14 @@ class Sprite {
 
   get frame() {
     return this.animations[this.currentAnimation][this.currentAnimationFrame];
+  }
+
+  setAnimation(key) {
+    if (this.currentAnimation !== key) {
+      this.currentAnimation = key;
+      this.currentAnimationFrame = 0;
+      this.animationFrameProgress = this.animationFrameLimit;
+    }
   }
 
   updateAnimationProgress() {
