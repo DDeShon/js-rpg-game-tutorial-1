0be97655310274d2922a1ad900769e19797c5aba
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-04 21:08:01 -0500

    working on overworld events

--- OverworldEvent.js
+++ OverworldEvent.js
@@ -3,4 +3,25 @@ class OverworldEvent {
     this.map = map;
     this.event = event;
   }
+
+  stand(resolve) {}
+
+  walk(resolve) {
+    const who = this.map.gameObjects[this.event.who];
+    who.startBehavior(
+      {
+        map: this.map,
+      },
+      {
+        type: "walk",
+        direction: this.event.direction,
+      }
+    );
+  }
+
+  init() {
+    return new Promise((resolve) => {
+      this[this.event.type](resolve);
+    });
+  }
 }
