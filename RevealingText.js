51a22e62dbe70f6d524ae892c5196a0cf6ef6bce
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-08 21:50:16 -0500

    working on RevealingText class

--- RevealingText.js
+++ RevealingText.js
@@ -5,5 +5,22 @@ class RevealingText {
     this.speed = config.element || 70;
 
     this.timeout = null;
+    this.isDone = false;
+  }
+
+  init() {
+    let characters = [];
+    this.text.split("").forEach((character) => {
+      // Create each span, add to element in DOM
+      let span = document.createElement("span");
+      span.textContent = character;
+      this.element.appendChild(span);
+
+      // Add this span to internal state Array
+      characters.push({
+        span,
+        delayAfter: character === " " ? 0 : this.speed,
+      });
+    });
   }
 }
