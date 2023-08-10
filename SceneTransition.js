27414981da9595e86124ff4ab4596d61115a3dbf
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-09 22:23:09 -0500

    set up SceneTransition class

--- SceneTransition.js
+++ SceneTransition.js
@@ -7,8 +7,27 @@ class SceneTransition {
     this.element.classList.add("SceneTransition");
   }
 
-  init(container) {
+  fadeOut() {
+    this.element.classList("fade-out");
+    this.element.addEventListener(
+      "onanimationend",
+      () => {
+        this.element.remove();
+      },
+      { once: true }
+    );
+  }
+
+  init(container, callback) {
     this.createElement();
     container.appendChild(this.element);
+
+    this.element.addEventListener(
+      "onanimationend",
+      () => {
+        callback();
+      },
+      { once: true }
+    );
   }
 }
