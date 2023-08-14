4f51f8dfbdae7052ecf78029dceef37fd0d2e074
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-14 07:12:42 -0500

    working on actions.js

--- Content/actions.js
+++ Content/actions.js
@@ -2,8 +2,9 @@ window.Actions = {
   damage1: {
     name: "whomp",
     success: [
-      { type: "textMessage", text: "Something happened!" },
-      { type: "animation", animation: "Something happened!" },
+      { type: "textMessage", text: "{CASTER} uses Whomp!" },
+      //   { type: "animation", animation: "Something happened!" },
+      //   { type: "stateChange", damage: 10 },
     ],
   },
 };
