ef1ce6336603e0c872ff16f7e891eb1a38f30e91
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-14 07:14:44 -0500

    setting up TurnCycle class

---
+++ Battle/TurnCycle.js
@@ -0,0 +1,11 @@
+class TurnCycle {
+  constructor({ battle, onNewEvent }) {
+    this.battle = battle;
+    this.onNewEvent = onNewEvent;
+    this.currentTeam = "player"; // or "enemy"
+  }
+
+  async turn() {}
+
+  init() {}
+}
