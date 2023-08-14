13575174b2c2b5900eea28156c51f7669f931cf6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-14 07:10:33 -0500

    setting up actions.js

---
+++ Content/actions.js
@@ -0,0 +1,9 @@
+window.Actions = {
+  damage1: {
+    name: "whomp",
+    success: [
+      { type: "textMessage", text: "Something happened!" },
+      { type: "animation", animation: "Something happened!" },
+    ],
+  },
+};
