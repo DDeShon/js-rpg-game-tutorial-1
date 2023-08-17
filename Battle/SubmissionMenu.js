af2cbde961d905244c61ee6d6718deb7ead073f3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-17 04:33:00 -0500

    working on player options UI

--- Battle/SubmissionMenu.js
+++ Battle/SubmissionMenu.js
@@ -5,6 +5,35 @@ class SubmissionMenu {
     this.onComplete = onComplete;
   }
 
+  getPages() {
+    return {
+      root: [
+        {
+          label: "Attack",
+          description: "Choose an attack",
+          handler: () => {
+            // Do something when chosen
+          },
+        },
+        {
+          label: "Items",
+          description: "Choose an item",
+          handler: () => {
+            // Go to items page
+          },
+        },
+        {
+          label: "Swap",
+          description: "Change to another pizza",
+          handler: () => {
+            // See pizza options
+          },
+        },
+      ],
+      attacks: [],
+    };
+  }
+
   decide() {
     this.onComplete({
       action: Actions[this.caster.actions[0]],
@@ -12,7 +41,14 @@ class SubmissionMenu {
     });
   }
 
+  showMenu(container) {}
+
   init(container) {
-    this.decide();
+    if (this.caster.isPlayerControlled) {
+      // Show UI
+      this.showMenu(container);
+    } else {
+      this.decide();
+    }
   }
 }
