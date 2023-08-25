b9b29f0552df909383ae23c802a8ebe3581f7e33
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-25 18:22:16 -0500

    working on PlayerState class

--- Battle/Battle.js
+++ Battle/Battle.js
@@ -61,11 +61,10 @@ class Battle {
       enemy: "enemy1",
     };
     this.items = [
-      { actionId: "item_recoverStatus", instanceId: "p1", team: "player" },
-      { actionId: "item_recoverStatus", instanceId: "p2", team: "player" },
-      { actionId: "item_recoverStatus", instanceId: "p3", team: "enemy" },
-
-      { actionId: "item_recoverHp", instanceId: "p4", team: "player" },
+      // { actionId: "item_recoverStatus", instanceId: "p1", team: "player" },
+      // { actionId: "item_recoverStatus", instanceId: "p2", team: "player" },
+      // { actionId: "item_recoverStatus", instanceId: "p3", team: "enemy" },
+      // { actionId: "item_recoverHp", instanceId: "p4", team: "player" },
     ];
   }
 
--- State/PlayerState.js
+++ State/PlayerState.js
@@ -1,5 +1,33 @@
 class PlayerState {
-  constructor() {}
+  constructor() {
+    this.pizzas = {
+      p1: {
+        pizzaId: "s001",
+        hp: 30,
+        maxHP: 50,
+        xp: 85,
+        maxXp: 100,
+        level: 1,
+        status: null,
+      },
+      p2: {
+        pizzaId: "v001",
+        hp: 50,
+        maxHP: 50,
+        xp: 85,
+        maxXp: 100,
+        level: 1,
+        status: null,
+      },
+    };
+    this.lineup = ["p1", "p2"];
+    this.items = [
+      { actionId: "item_recoverStatus", instanceId: "item1" },
+      { actionId: "item_recoverHp", instanceId: "item2" },
+      { actionId: "item_recoverHp", instanceId: "item3" },
+    ];
+  }
 
   init() {}
 }
+window.playerState = new PlayerState();
