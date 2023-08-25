46d53d01c984735dbaddcacc0534d364c7fc144c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-25 18:25:08 -0500

    working on enemies.js

--- Content/enemies.js
+++ Content/enemies.js
@@ -1,1 +1,18 @@
-window.Enemies = {};
+window.Enemies = {
+  erio: {
+    name: "Erio",
+    src: "/images/characters/people/erio.png",
+    pizzas: {
+      a: {
+        pizzaId: "s001",
+        maxHP: 50,
+        level: 1,
+      },
+      b: {
+        pizzaId: "s002",
+        maxHP: 50,
+        level: 1,
+      },
+    },
+  },
+};
