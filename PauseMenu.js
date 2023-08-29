107a8e1d0accf06b6378df6f1140ea94d517e46e
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-28 20:45:12 -0500

    working on PauseMenu class

--- PauseMenu.js
+++ PauseMenu.js
@@ -1,5 +1,35 @@
 class PauseMenu {
-  constructor() {}
+  constructor(onComplete) {
+    this.onComplete = onComplete;
+  }
 
-  init() {}
+  getOptions() {}
+
+  createElement() {
+    this.element = document.createElement("div");
+    this.element.classList.add("PauseMenu");
+    this.element.innerHTML = `
+        <h2>Pause Menu</h2>
+    `;
+  }
+
+  close() {
+    //
+  }
+
+  async init(container) {
+    this.createElement();
+    this.keyboardMenu = new KeyboardMenu({
+      //
+    });
+    this.keyboardMenu.init(this.element);
+    this.keyboardMenu.setOptions([]);
+
+    container.appendChild(this.element);
+
+    utils.wait(200);
+    this.esc = new KeyPressListener("Escape", () => {
+      this.close();
+    });
+  }
 }
