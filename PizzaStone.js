a45eacfa66da49f213c50cf417b714f6002598a0
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-08-31 18:43:53 -0500

    working on PizzaStone.js

--- PizzaStone.js
+++ PizzaStone.js
@@ -1,5 +1,16 @@
-class PizzaStone {
-  constructor() {}
+class PizzaStone extends GameObject {
+  constructor(config) {
+    super(config);
+    this.sprite = new Sprite({
+      gameObject: this,
+      src: "/images/characters/pizza-stone.png",
+      animations: {
+        "used-down": [[0, 0]],
+        "unused-down": [[1, 0]],
+      },
+      currentAnimation: "used-down",
+    });
+  }
 
   init() {}
 }
