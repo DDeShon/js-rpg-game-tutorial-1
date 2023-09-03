d9c88c905390d110d9110dbc91820162b6431b5c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-03 18:18:24 -0500

    working on Progress.js

--- Progress.js
+++ Progress.js
@@ -25,7 +25,21 @@ class Progress {
     );
   }
 
-  getSaveFile() {}
+  getSaveFile() {
+    const file = window.localStorage.getItem(this.saveFileKey);
+    return file ? JSON.parse(file) : null;
+  }
 
-  load() {}
+  load() {
+    const file = this.getSaveFile();
+    if (file) {
+      this.mapId = file.mapId;
+      this.startingHeroX = file.startingHeroX;
+      this.startingHeroY = file.startingHeroY;
+      this.startingHeroDirection = file.startingHeroDirection;
+      Object.keys(file.playerState).forEach((key) => {
+        playerState[key] = file.playerState[key];
+      });
+    }
+  }
 }
