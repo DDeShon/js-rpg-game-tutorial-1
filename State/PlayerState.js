2312e594b4df785d9a99d6875a0451f2ca1de008
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-02 21:49:52 -0500

    working on CraftingMenu.js

--- State/PlayerState.js
+++ State/PlayerState.js
@@ -40,7 +40,23 @@ class PlayerState {
     };
   }
 
-  addPizza(pizzaId) {}
+  addPizza(pizzaId) {
+    const newId = `p${Date.now()}` + Math.floor(Math.random() * 99999);
+    this.pizzas[newId] = {
+      pizzaId,
+      hp: 50,
+      maxHP: 50,
+      xp: 0,
+      maxXp: 100,
+      level: 1,
+      status: null,
+    };
+    if (this.lineup.length < 3) {
+      this.lineup.push(newId);
+    }
+    utils.emitEvent("LineupChanged");
+    console.log(this);
+  }
 
   swapLineup(oldId, incomingId) {
     const oldIndex = this.lineup.indexOf(oldId);
