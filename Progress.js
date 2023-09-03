e7080fcc8bdd8215fe5a880609c3714ae792d07d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-03 18:08:08 -0500

    setting up Progress.js

--- Progress.js
+++ Progress.js
@@ -1,3 +1,14 @@
 class Progress {
-  constructor() {}
+  constructor() {
+    this.mapId = "DemoRoom";
+    this.startingHeroX = 0;
+    this.startingHeroY = 0;
+    this.startingHeroDirection = "down";
+  }
+
+  save() {}
+
+  getSaveFile() {}
+
+  load() {}
 }
