85ac806ededab0384557e2e12f0bafe08a6e10ad
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-03 18:33:11 -0500

    working on Progress.js

--- OverworldEvent.js
+++ OverworldEvent.js
@@ -93,7 +93,7 @@ class OverworldEvent {
   pause(resolve) {
     this.map.isPaused = true;
     const menu = new PauseMenu({
-      progress: this.map.overworl.progress,
+      progress: this.map.overworld.progress,
       onComplete: () => {
         resolve();
         this.map.isPaused = false;
--- overworld.js
+++ overworld.js
@@ -86,19 +86,36 @@ class Overworld {
   }
 
   init() {
+    // Create a new Progress tracker
     this.progress = new Progress();
 
+    // Check for and load saved data
+    let initialHeroState = null;
+    const saveFile = this.progress.getSaveFile();
+    if (saveFile) {
+      this.progress.load();
+      initialHeroState = {
+        x: this.progress.startingHeroX,
+        y: this.progress.startingHeroY,
+        direction: this.progress.startingHeroDirection,
+      };
+    }
+
+    // Load the HUD
     this.hud = new Hud();
     this.hud.init(document.querySelector(".game-container"));
 
-    this.startMap(window.OverworldMaps.Street);
+    // Start the first map
+    this.startMap(window.OverworldMaps(this.progress.mapId), initialHeroState);
 
+    // Create controls
     this.bindActionInput();
     this.bindHeroPositionCheck();
 
     this.directionInput = new DirectionInput();
     this.directionInput.init();
 
+    // Start the game
     this.startGameLoop();
 
     // this.map.startCutscene([
