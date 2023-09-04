603adc8ece90662ee6f2b9e6422b27b0284c1cff
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-09-04 17:30:18 -0500

    working on the title screen

--- TitleScreen.js
+++ TitleScreen.js
@@ -1,13 +1,26 @@
 class TitleScreen {
-  constructor() {}
+  constructor({ progress }) {
+    this.progress = progress;
+  }
 
-  getOptions() {
-    return [];
+  getOptions(resolve) {
+    return [
+      {
+        label: "New Game",
+        description: "Start a new pizza adventure!",
+        handler: () => {
+          //TODO:
+        },
+      },
+    ];
   }
 
   createElement() {
     this.element = document.createElement("div");
     this.element.classList.add("TitleScreen");
+    this.element.innerHTML = `
+        <img class="TitleScreen_logo" src="/images/logo.png" alt="Pizza Legends" />
+    `;
   }
 
   close() {
@@ -15,5 +28,13 @@ class TitleScreen {
     this.element.remove();
   }
 
-  init(container) {}
+  init(container) {
+    return new Promise((resolve) => {
+      this.createElement();
+      container.appendChild(this.element);
+      this.keyboardMenu = new KeyboardMenu();
+      this.keyboardMenu.init(this.element);
+      this.keyboardMenu.setOptions(this.getOptions(resolve));
+    });
+  }
 }
